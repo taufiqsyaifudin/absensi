9a88bc32088078fdcf2eeb8a025d234c014a748f
taufiqsyaifudin <taufiqsyaifudin.dev@gmail.com>
2022-05-26 13:09:52 +0700

    menambahkan middleware

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\LoginController;
+use GuzzleHttp\Middleware;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-route::get('/home',[HomeController::class,'index']);
+route::get('/login',[LoginController::class,'halamanlogin'])->name('login');
+route::post('/postlogin',[LoginController::class,'postlogin'])->name('postlogin');
+route::get('/logout',[LoginController::class,'logout'])->name('logout');
+
+route::group(['middleware' => ['auth']], function () {
+    route::get('/home',[HomeController::class,'index'])->name('home');
+});
+
