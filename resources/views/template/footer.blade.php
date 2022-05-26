670393a2214d29353cedbe176d7d7597bbfac0f0
taufiqsyaifudin <taufiqsyaifudin.dev@gmail.com>
2022-05-26 11:16:53 +0700

    menambahkan template admin LTE

---
+++ resources/views/template/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Taufiq Syaifudin
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="https://adminlte.io">Aapliaksi Absebsi</a>.</strong> All rights reserved.
+</footer>
